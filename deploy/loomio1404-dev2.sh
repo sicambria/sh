d971829923e6584d6b83746bc817b60be9168326
sicambria <sicambria@users.noreply.github.com>
2016-04-26 16:49:17 +0200

    Update loomio1404-dev2.sh
    Part 2 setup extended

--- deploy/loomio1404-dev2.sh
+++ deploy/loomio1404-dev2.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
+echo "PART 2 - Setting up Loomio development environment for Ubuntu 14.04 x64"
+
 cd ~/projects/loomio
-cp config/database.example.yml config/database.yml
 bundle install
+cp config/database.example.yml config/database.yml
 bundle exec rake db:setup
 
-sudo npm install -g bower
-sudo npm install -g gulp
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE loomio_development to `cat /tmp/whoami.log` ;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE loomio_test to `cat /tmp/whoami.log` ;"
+sudo su postgres -c "psql loomio_development -c 'CREATE EXTENSION hstore;'"
+sudo su postgres -c "psql loomio_test -c 'CREATE EXTENSION hstore;'"
 
-rails s
-echo "You should now be able to browse to your localhost Loomio: http://localhost:3000"
+bundle exec rake db:setup
+bundle exec rake bootstrap > log_rake_bootstrap.log
+grep 'Created user with email' log_rake_bootstrap.log > password.txt
+
+echo "LOOMIO LOGIN:"
+cat password.txt
+
+echo
+echo "localhost address for Loomio: http://localhost:3000"
+echo "To create a sample discussion, run: localhost:3000/development/setup_discussion"
+
+cd ~/projects/loomio/angular
+gulp dev
+
+# If you have an issue with gulp, then run these 2 commands, then run this script again:
+# cd
+# rm -rf .npm
+
+# Start server:
+# rails s
