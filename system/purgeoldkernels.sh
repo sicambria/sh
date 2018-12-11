1020e11ee5659706caa9ae92fceb78a57831c167
sicambria <sicambria@users.noreply.github.com>
2018-12-11 10:38:36 +0100

    Create purgeoldkernels.sh

---
+++ system/purgeoldkernels.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Purge old kernels? Press CTRL-C to STOP or ENTER to continue."
+read
+echo "START DRY RUN"
+dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | grep -E "(image|headers)" | xargs sudo dpkg --remove --dry-run
+echo "All is OK? Press CTRL-C to STOP or ENTER to remove the kernels listed above."
+read
+dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e `uname -r | cut -f1,2 -d"-"` | grep -e [0-9] | grep -E "(image|headers)" | xargs sudo dpkg --remove
+
+# Thanks to http://tuxtweaks.com/2010/10/remove-old-kernels-in-ubuntu-with-one-command/
